aa23a0961244c66921af534bac7bac5f6391f0a9
kenza <menadkenza2022@gmail.com>
2025-11-03 17:05:11 +0100

    Ajout du fichier devinette.c

--- devinette.c
+++ devinette.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
-    printf("Hello World\n");
+int main(void) {
+    int secret, guess, essais = 0;
+    srand(time(NULL));
+    secret = rand() % 100 + 1;
+
+    printf("=== Jeu de devinette ===\n");
+    do {
+        printf("Devine le nombre (1 à 100) : ");
+        scanf("%d", &guess);
+        essais++;
+        if (guess < secret) printf("➡️ Plus grand !\n");
+        else if (guess > secret) printf("⬇️ Plus petit !\n");
+        else printf("🎉 Bravo ! Trouvé en %d essais.\n", essais);
+    } while (guess != secret);
+
     return 0;
 }
+
